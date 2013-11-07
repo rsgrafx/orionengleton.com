1a3f775f023ac51a77ada99c921f3178bac542f8
Orion Engleton - MBAir <orion.engleton@gmail.com>
2013-11-07 16:22:15 -0500

    Initial Commit of my application

---
+++ index.js
@@ -0,0 +1,6 @@
+var orionApp = angular.module('OrionApplication', []);
+
+orionApp.controller('BaseCtrl', ['$scope', function BaseCtrl($scope) {
+    $scope.title = "Learning to Dream in Color and Code. | Orion Engleton";
+  }]
+);
