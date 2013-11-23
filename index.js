93d2d4a8b942bb2dfc48f90838cbcfb645915cbe
Orion Engleton - MBAir <orion.engleton@gmail.com>
2013-11-23 12:30:07 -0500

    Fixed attrs check for www infront of attrs.url Added the photos directive

--- index.js
+++ index.js
@@ -42,17 +42,35 @@ orionApp.directive('clip', function() {
     // template: '<video controls poster="default" src={{url}}></video>',
     link: function(scope, element, attrs) {
       myE = $(element);
-      if (attrs.site == 'youtube.com') {
-        var tag = '<iframe width="450" height="320" src="' + attrs.url +'" frameborder="0" allowfullscreen></iframe>'
+      if (attrs.site == 'youtube.com' || attrs.site == 'www.youtube.com') {
+        var tag = '<iframe width="450" height="320" src="' + attrs.url +'" frameborder="0" allowfullscreen></iframe> <br/>'
         myE.append(tag) 
+      } else if (attrs.site == 'vimeo.com' || attrs.site =='www.vimeo.com') {
+        var tag = '<iframe src="' +attrs.url+ '"  width="450" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <br/>'
+                
+
+                  // <iframe src="//player.vimeo.com/video/76164532" 
+                  // width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen 
+                  // allowfullscreen></iframe> <p><a href="http://vimeo.com/76164532">
+                  // Adventure Time Full Episode - The Vault</a> from <a href="http://vimeo.com/naytaviabrown">
+                  // Naytavia Brown</a> on <a href="https://vimeo.com">Vimeo</a>.</p> -->
+
+        myE.append(tag)
       } else {
-        
         var tag = ' <video controls poster="' + attrs.poster + '" width="450" height="320"> ' + 
         ' <source src="'+attrs.url +'" type="video/mp4"> '+
         '<a href="'+attrs.url+'">Download song</a> </video>'
         myE.append(tag)
       }
     }
+  }
+})
+
+orionApp.directive('photo', function() {
+  return {
+    restrict: 'AE',
+    scope: { url: '@url', site: '@site' },
+    template: '<img src="http://{{url}}"" alt="{{site}}" />'
   }
 })
 
